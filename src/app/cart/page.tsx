684595c29438daefbeaf23e1b39aa6871646eb2d
javeria-fatima49 <fatimajaveria409@gmail.com>
2025-02-09 17:05:20 +0500

    hackathon push to github

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -774,7 +774,7 @@ const Cart = () => {
     setError(null)
 
     try {
-      const response = await fetch("http://localhost:3002/api/orders", {
+      const response = await fetch("http://localhost:3000/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
